b23d67a86eb1abe52da9b85e94db8e08010223ce
Felipinbombin <cephei.1313@gmail.com>
2021-01-30 01:11:16 -0300

    resize circle on map

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ let config = {
     // Pairs of zoom level - radius of ball (in pixels). Gets interpolated
     12, 1.5,
     14, 4,
-    20, 40,
+    20, 10,
   ],
   stop_creation_zoom: [
     12, 1.5,
@@ -23,13 +23,13 @@ let config = {
   shape_point_zoom: [
     12, 1.5,
     14, 4,
-    20, 40,
+    20, 10,
   ],
 
   stoptimes_stop_zoom: [
     12, 1.5,
     14, 4,
-    20, 40,
+    20, 10,
   ],
 
 }
