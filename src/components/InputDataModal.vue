90519820968e157af8f18f924137fd543dd9a8dc
Felipinbombin <cephei.1313@gmail.com>
2021-02-04 16:51:03 -0300

    added new modals

---
+++ src/components/InputDataModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="modal-container" @click="$emit('close')">
+    <div class="modal" :class="modalClasses" @click.stop>
+      <slot name="mcontent"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseModal',
+  props: {
+    modalClasses: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
