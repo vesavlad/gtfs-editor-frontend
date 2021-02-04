252e71705a955f5a83f38de504bf213711069b7f
Felipinbombin <cephei.1313@gmail.com>
2021-02-04 13:27:29 -0300

    fixed url

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,32 +1,32 @@
 <template>
-    <a href="#" class="card project-card">
-        <div class="project-card-map">
-            <EnvelopeMap :project="project"></EnvelopeMap>
+  <router-link class="card project-card" :to="{name: 'projectoverview', params: {projectid: project.project_id}}">
+    <div class="project-card-map">
+      <EnvelopeMap :project="project"></EnvelopeMap>
+    </div>
+    <div class="card-content">
+      <div class="grid title">
+        <h2>{{project.name}}</h2>
+        <button class="btn icon flat"><i class="material-icons">more_vert</i></button>
+      </div>
+      <div class="grid project-details">
+        <div class="project-id">
+          <strong>
+            <span>ID:</span>
+            <span>{{project.project_id}}</span>
+          </strong>
         </div>
-        <div class="card-content" @click="$router.push({ name: 'projectoverview', params: { projectid: project.project_id } })">
-            <div class="grid title">
-                <h2>{{project.name}}</h2>
-                <button class="btn icon flat"><i class="material-icons">more_vert</i></button>
-            </div>
-            <div class="grid project-details">
-                <div class="project-id">
-                    <strong>
-                        <span>ID:</span>
-                        <span>{{project.project_id}}</span>
-                    </strong>
-                </div>
-                <div class="project-last-edit">
-                    <span>{{ $t('projectCard.lastChange') }}: </span>
-                    <span>{{ lastModification }}</span>
-                </div>
-            </div>
-            <div class="grid-pills">
-                <PillBase v-if="project.gtfsvalidation" pillClass="error" :pillText="$t('projectCard.errors') + ': ' + project.gtfsvalidation.error_number"></PillBase>
-                <PillBase v-if="project.gtfsvalidation" pillClass="warning" :pillText="$t('projectCard.warnings')+': ' + project.gtfsvalidation.warning_number"></PillBase>
-                <PillBase v-if="!project.gtfsvalidation" pillClass="empty" :pillText="'without validation'"></PillBase>
-            </div>
+        <div class="project-last-edit">
+          <span>{{ $t('projectCard.lastChange') }}: </span>
+          <span>{{ lastModification }}</span>
         </div>
-    </a>
+      </div>
+      <div class="grid-pills">
+        <PillBase v-if="project.gtfsvalidation" pillClass="error" :pillText="$t('projectCard.errors') + ': ' + project.gtfsvalidation.error_number"></PillBase>
+        <PillBase v-if="project.gtfsvalidation" pillClass="warning" :pillText="$t('projectCard.warnings')+': ' + project.gtfsvalidation.warning_number"></PillBase>
+        <PillBase v-if="!project.gtfsvalidation" pillClass="empty" :pillText="'without validation'"></PillBase>
+      </div>
+    </div>
+  </router-link>
 </template>
 
 <script>
