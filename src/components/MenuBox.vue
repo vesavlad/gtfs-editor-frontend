5ba2fc280cba1dbe6776d03498c2fa76390c3f16
Felipinbombin <cephei.1313@gmail.com>
2021-02-09 18:14:43 -0300

    refactoring

--- src/components/MenuBox.vue
+++ src/components/MenuBox.vue
@@ -1,24 +1,24 @@
 <template>
-    <div class="menu-box card">
-      <ul>
-        <li class="menu-option">
-          <i class="material-icons">edit</i>
-          <span>Edit entire shape</span>
-        </li>
-        <li class="menu-option disabled">
-          <i class="material-icons">timeline</i>
-          <span>Edit entire shape</span>
-        </li>
-        <li class="menu-option">
-          <i class="material-icons">content_copy</i>
-          <span>Duplicate</span>
-        </li>
-        <li class="menu-option delete">
-          <i class="material-icons">delete</i>
-          <span>Delete</span>
-        </li>
-      </ul>
-    </div>
+  <div class="menu-box card">
+    <ul>
+      <li class="menu-option">
+        <i class="material-icons">edit</i>
+        <span>Edit entire shape</span>
+      </li>
+      <li class="menu-option disabled">
+        <i class="material-icons">timeline</i>
+        <span>Edit entire shape</span>
+      </li>
+      <li class="menu-option">
+        <i class="material-icons">content_copy</i>
+        <span>Duplicate</span>
+      </li>
+      <li class="menu-option delete">
+        <i class="material-icons">delete</i>
+        <span>Delete</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
