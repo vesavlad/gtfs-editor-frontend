3afbe6072325e4d12fc953faf22cb8fb7be3179d
Felipinbombin <cephei.1313@gmail.com>
2021-02-16 18:01:44 -0300

    added new style to local switcher

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -1,37 +1,47 @@
 <template>
   <ul class="language-switch">
-    <li v-for="locale in locales" :key="locale" @click="switchLocale(locale)">
-      <span>{{locale}}</span>
+    <li v-for="locale in locales" :key="locale" @click="switchLocale(locale)" :class="{active: locale===$i18n.locale}">
+      <span>{{ getFlag(locale) }} {{ locale }}</span>
     </li>
   </ul>
 </template>
 
 
 <script>
-  import { LANGUAGE_KEY } from '@/utils/consts.js';
+import {LANGUAGE_KEY} from '@/utils/consts.js';
 
-  export default {
-    name: 'LocaleSwitcher',
-    data() {
-      return {
-        locales: process.env.VUE_APP_I18N_SUPPORTED_LOCALE.split(',')
+export default {
+  name: 'LocaleSwitcher',
+  data() {
+    return {
+      locales: process.env.VUE_APP_I18N_SUPPORTED_LOCALE.split(',')
+    }
+  },
+  methods: {
+    switchLocale(locale) {
+      if (this.$i18n.locale !== locale) {
+        this.$i18n.locale = locale;
+        window.localStorage.setItem(LANGUAGE_KEY, locale);
       }
     },
-    methods: {
-      switchLocale(locale) {
-        if (this.$i18n.locale !== locale) {
-          this.$i18n.locale = locale;
-          window.localStorage.setItem(LANGUAGE_KEY, locale);
-        }
+    getFlag(locale) {
+      switch (locale) {
+        case 'es':
+          return '🇪🇸'
+        case 'en':
+          return '🇺🇸'
+        default:
+          return ''
       }
-    },
-  }
+    }
+  },
+}
 </script>
 
 <style scoped>
-  li {
-    text-decoration: underline;
-    color: #459CE7;
-    cursor: pointer;
-  }
+li {
+  text-decoration: underline;
+  color: #459CE7;
+  cursor: pointer;
+}
 </style>
