a2035021cb859d9f889e60d064087e57a64abc57
Felipinbombin <cephei.1313@gmail.com>
2021-02-23 00:54:58 -0300

    use multiselect in SimpleSelect.vue

--- src/components/vuetable/SimpleSelect.vue
+++ src/components/vuetable/SimpleSelect.vue
@@ -1,16 +1,16 @@
 <template>
-  <select class="select-simple" ref="select" v-model="val">
-    <option v-for="(option, key) in field.options" :key="key" :value="option">{{ key }}</option>
-  </select>
+  <Multiselect v-model="val" :options="field.options" track-by="value" label="name" @change="onChange"></Multiselect>
 </template>
 
-
 <script>
+import Multiselect from 'vue-multiselect';
 import fieldMixin from "@/mixins/fieldMixin.js";
-import $ from 'jquery';
-import 'select2';
 
 export default {
+  name: 'SimpleSelect',
+  components: {
+    Multiselect
+  },
   mixins: [
     fieldMixin,
   ],
@@ -19,46 +19,30 @@ export default {
       type: Object,
       required: true,
     },
-    value: {},
+    value: {
+      required: true
+    },
     hasErrors: {
       type: Boolean,
       default: false,
     },
   },
-  watch: {
-    value() {
-      this.changeEnabled = false;
-      $(this.$refs.select).select2('destroy');
-      this.val = (this.value === "") ? null : this.value;
-      this.$nextTick(this.datafy);
-    }
-  },
   data() {
     return {
-      changeEnabled: true,
       val: this.value,
       name: this.getFieldName(this.field),
     }
   },
-  mounted() {
-    this.datafy();
-    $(this.$refs.select).on('change', this.onChange);
+  watch: {
+    value() {
+      this.val = this.value === "" ? null : this.value;
+    }
   },
   methods: {
     onChange(evt) {
       this.val = evt.target.value;
-      if (this.changeEnabled) {
-        this.$emit("input", this.val);
-      }
-    },
-    log(msg) {
-      console.log(msg);
-    },
-    datafy() {
-      let select = this.$refs.select;
-      $(select).select2();
-      this.changeEnabled = true;
-    },
+      this.$emit("input", this.val);
+    }
   },
 }
 </script>
