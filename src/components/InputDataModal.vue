b41b55ef0590da36f6b6c8b06c684d51bdf562be
Felipinbombin <cephei.1313@gmail.com>
2021-02-24 02:29:52 -0300

    add generalized input component to InputDataModal.vue

--- src/components/InputDataModal.vue
+++ src/components/InputDataModal.vue
@@ -11,17 +11,20 @@
       <ul class="m-content">
         <li v-for="field in fields" :key="field.name">
           <div class="input-row-title h4" :class="{required: field.required}">
-            <span>{{ field.label }}{{ field.required?'*':'' }}</span>
+            <span>{{ field.title }}{{ field.required?'*':'' }}</span>
           </div>
           <div class="input-row-content single">
             <template v-if="[InputType.INPUT, InputType.DATE, InputType.URL, InputType.EMAIL].indexOf(field.type)>-1">
-              <input :type="field.type" v-model="localData[field.name]" :placeholder="`Enter ${field.label.toLowerCase()}`" @focus="$emit('removeError', field.name)" :class="{error: errors[field.name]}" v-tooltip="{ theme: 'error-tooltip', content: errors[field.name]?errors[field.name][0]:'', shown: errors[field.name]!==undefined }"/>
+              <input :type="field.type" v-model="localData[field.name]" :placeholder="`Enter ${field.title.toLowerCase()}`" @focus="$emit('removeError', field.name)" :class="{error: errors[field.name]}" v-tooltip="{ theme: 'error-tooltip', content: errors[field.name]?errors[field.name][0]:'', shown: errors[field.name]!==undefined }"/>
             </template>
             <template v-else-if="field.type===InputType.CHECKBOX">
               <label>
                 <div class="checkbox"></div>
                 <input type="checkbox" v-model="localData[field.name]" />
               </label>
+            </template>
+            <template v-else-if="[InputType.SIMPLE_SELECT, InputType.FK_SELECT, InputType.COLOR].indexOf(field.type)>-1">
+              <GeneralizedInput v-model="localData[field.name]" :field="field" :data="{}" @focus="$emit('removeError', field.name)" v-tooltip="{ theme: 'error-tooltip', content: errors[field.name]?errors[field.name][0]:'', shown: errors[field.name]!==undefined }"></GeneralizedInput>
             </template>
           </div>
         </li>
@@ -42,11 +45,13 @@
 <script>
 import BaseModal from '@/components/BaseModal.vue';
 import Enums from "@/utils/enums";
+import GeneralizedInput from "@/components/vuetable/GeneralizedInput";
 
 export default {
   name: 'InputDataModal',
   components: {
-    BaseModal
+    BaseModal,
+    GeneralizedInput
   },
   props: {
     classes: {
@@ -65,15 +70,16 @@ export default {
       type: Array,
       default: () => [],
       validator(value) {
-        let properties = ['name', 'required', 'label', 'type'];
+        // required field is optional so is not checked
+        let properties = ['name', 'title', 'type'];
         for (let i = 0; i < properties.length; i++) {
           for (let j = 0; j < value.length; j++) {
             if (!Object.prototype.hasOwnProperty.call(value[j], properties[i])) {
               console.warn(`Property ${properties[i]} is not present in element ${j+1} of fields prop`);
               return false;
             }
-            if (properties[i]==='type' && Object.values(Enums.InputType).indexOf(value[j])===-1) {
-              console.warn(`value ${value[j]} is not valid for property ${properties[i]} in element ${j+1}`);
+            if (properties[i]==='type' && Object.values(Enums.InputType).concat([null]).indexOf(value[j][properties[i]])===-1) {
+              console.warn(`value ${value[j][properties[i]]} is not valid for property ${properties[i]} in element ${j+1}`);
               return false;
             }
           }
