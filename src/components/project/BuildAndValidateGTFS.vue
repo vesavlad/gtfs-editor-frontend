705cef60c2531b2d95816e6091cec1137b274d17
Felipinbombin <cephei.1313@gmail.com>
2021-03-03 17:04:11 -0300

    implement accordion in validation gtfs report

--- src/components/project/BuildAndValidateGTFS.vue
+++ src/components/project/BuildAndValidateGTFS.vue
@@ -65,19 +65,20 @@
         <div class="m-header">
           <h2>{{ $t('projectDashboard.gtfsBuilder.validationReport.title') }}</h2>
           <div class="grid">
-            <a class="btn icon flat" target="_blank" href="https://gtfs.mobilitydata.org/spec/gtfs-schedule"><i class="material-icons">info</i></a>
+            <a class="btn icon flat" target="_blank" href="https://gtfs.mobilitydata.org/spec/gtfs-schedule"><i
+                class="material-icons">info</i></a>
             <button class="btn icon flat" @click="showModal=false"><i class="material-icons">close</i></button>
           </div>
         </div>
-        <ul class="report-list m-content">
-          <li class="report-collapse">
-            <button class="report-header">
+        <transition-group class="report-list m-content" name="flip-list" tag="ul">
+          <li class="report-collapse" :class="{collapsed: report.activeRow!==file.filename || !report.show}" v-for="(file, index) in errorData" :key="index">
+            <button class="report-header" @click="setVisibility(file)">
               <div>
                 <i class="material-icons">description</i>
-                <h3>routes.txt</h3>
+                <h3>{{ file.filename }}</h3>
                 <div class="grid min">
-                  <pillBase pillClass="error" pillText="3"></pillBase>
-                  <pillBase pillClass="warning" pillText="5"></pillBase>
+                  <pillBase pillClass="error" :pillText="file.errorNumber"></pillBase>
+                  <pillBase pillClass="warning" :pillText="file.warningNumber"></pillBase>
                 </div>
               </div>
               <div class="icon flat">
@@ -87,104 +88,27 @@
             <div class="report-body">
               <div class="report-content">
                 <ul class="report-box errors">
-                  <li>
-                    <h4>`route_color` and `route_text_color` have insufficient contrast</h4>
-                    <span>Code: 8, Entity ID: L</span>
-                    <p>"Contrast ratio should be >= 4.5 but was `1.4932792189163142` for route:`C` in file:`routes.txt`.
-                      The `route_text_color` and `route_color` should be set to contrasting colors, as they are used as
-                      the text and background color (respectively) for displaying route names. When left blank,
-                      `route_text_color` defaults to 000000 (black) and `route_color` defaults to FFFFFF (white). A
-                      common source of issues here is setting `route_color` to a dark color, while leaving
-                      `route_text_color` set to black. In this case, `route_text_color` should be set to a lighter color
-                      like FFFFFF to ensure a legible contrast between the two. The contrast ratio formula used can be
-                      found here : https://www.w3.org/TR/WCAG20-TECHS/G17.html#G17-procedure"</p>
-                  </li>
-                  <li>
-                    <h4>Title</h4>
-                    <span>Code, Entity ID</span>
-                    <p>description</p>
-                  </li>
-                  <li>
-                    <h4>Title</h4>
-                    <span>Code, Entity ID</span>
-                    <p>description</p>
+                  <li v-for="(row, index) in file.errors" :key="index">
+                    <h4>{{ row.title }}</h4>
+                    <span>Code: {{ row.code }}, Entity ID: {{ row.entityId }}</span>
+                    <p>{{ row.description }}</p>
                   </li>
                 </ul>
                 <ul class="report-box warnings">
-                  <li>
-                    <h4>Title</h4>
-                    <span>Code, Entity ID</span>
-                    <p>description</p>
-                  </li>
-                  <li>
-                    <h4>Title</h4>
-                    <span>Code, Entity ID</span>
-                    <p>description</p>
-                  </li>
-                  <li>
-                    <h4>Title</h4>
-                    <span>Code, Entity ID</span>
-                    <p>description</p>
+                  <li v-for="(row, index) in file.warnings" :key="index">
+                    <h4>{{ row.title }}</h4>
+                    <span>Code: {{ row.code }}, Entity ID: {{ row.entityId }}</span>
+                    <p>{{ row.description }}</p>
                   </li>
                 </ul>
               </div>
             </div>
           </li>
-          <li class="report-collapse collapsed">
-            <button class="report-header">
-              <div>
-                <i class="material-icons">description</i>
-                <h3>filename</h3>
-                <pillBase pillClass="error" pillText="3"></pillBase>
-                <pillBase pillClass="warning" pillText="5"></pillBase>
-              </div>
-              <div class="icon flat">
-                <i class="material-icons">keyboard_arrow_down</i>
-              </div>
-            </button>
-            <div class="report-body">
-              <div class="report-content">
-                <ul class="report-box errors">
-                  <li>
-                    <h4>Title</h4>
-                    <span>Code, Entity ID</span>
-                    <p>description</p>
-                  </li>
-                  <li>
-                    <h4>Title</h4>
-                    <span>Code, Entity ID</span>
-                    <p>description</p>
-                  </li>
-                  <li>
-                    <h4>Title</h4>
-                    <span>Code, Entity ID</span>
-                    <p>description</p>
-                  </li>
-                </ul>
-                <ul class="report-box warnings">
-                  <li>
-                    <h4>Title</h4>
-                    <span>Code, Entity ID</span>
-                    <p>description</p>
-                  </li>
-                  <li>
-                    <h4>Title</h4>
-                    <span>Code, Entity ID</span>
-                    <p>description</p>
-                  </li>
-                  <li>
-                    <h4>Title</h4>
-                    <span>Code, Entity ID</span>
-                    <p>description</p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </li>
-        </ul>
+        </transition-group>
         <div class="m-footer">
           <div class="option-buttons">
-            <button class="btn"><span>{{ $t('projectDashboard.gtfsBuilder.validationReport.downloadCSV') }}</span></button>
+            <button class="btn"><span>{{ $t('projectDashboard.gtfsBuilder.validationReport.downloadCSV') }}</span>
+            </button>
           </div>
         </div>
       </template>
@@ -215,12 +139,59 @@ export default {
     return {
       showModal: false,
       interval: null,
-      status: Enums.BuildAndValidateGTFS
+      status: Enums.BuildAndValidateGTFS,
+      report: {
+        activeRow: null,
+        show: true
+      }
     }
   },
   computed: {
     lastBuildExecution() {
       return DateTime.fromISO(this.project.gtfs_file_updated_at).toRelative({locale: this.$i18n.locale});
+    },
+    errorData() {
+      let rows = this.project.gtfs_validation.message.split('\n').map(row => row.split(','));
+      rows.shift();
+      return rows.reduce((accumulator, row, index) => {
+        let filename = row[0];
+        let code = row[1];
+        let level = row[2];
+        let entityId = row[3];
+        let title = row[4];
+        let description = row[5];
+
+        if (index===0) {
+          this.report.activeRow = filename;
+          this.report.show = true;
+        }
+
+        if (!accumulator[filename]) {
+          accumulator[filename] = {
+            filename: filename,
+            errorNumber: 0,
+            warningNumber: 0,
+            errors: [],
+            warnings: []
+          };
+        }
+
+        let data = {
+          title,
+          description,
+          code,
+          entityId
+        }
+        if (level === 'ERROR') {
+          accumulator[filename].errorNumber += 1;
+          accumulator[filename].errors.push(data);
+        } else if (level === 'WARNING') {
+          accumulator[filename].warningNumber += 1;
+          accumulator[filename].warnings.push(data);
+        }
+
+        return accumulator;
+      }, {});
     }
   },
   methods: {
@@ -261,6 +232,14 @@ export default {
           }
         });
       }, 2000);
+    },
+    setVisibility(file) {
+      if (this.report.activeRow!==file.filename) {
+        this.report.activeRow = file.filename;
+        this.report.show = true;
+      } else {
+        this.report.show=!this.report.show;
+      }
     }
   },
   beforeDestroy() {
