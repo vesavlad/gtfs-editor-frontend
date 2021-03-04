23ff50e3e94a7c2a2845365930e4b56d03a5ba21
Felipinbombin <cephei.1313@gmail.com>
2021-03-04 00:57:32 -0300

    detect horizontal scroll

--- src/components/vuetable/SimpleSelect.vue
+++ src/components/vuetable/SimpleSelect.vue
@@ -56,22 +56,28 @@ export default {
       ref.$refs.tags.style.width = null;
     },
     repositionDropDown() {
-      const {top, height,left} = this.$el.getBoundingClientRect();
+      const {top, height, left} = this.$el.getBoundingClientRect();
       const ref = this.$refs.multiselect;
-      //ref.$refs.tags.style.width = `${width}px`;
-      //ref.$refs.list.style.width = `${width}px`;
-      ref.$refs.list.style.position = 'fixed';
-      ref.$refs.list.style.bottom = 'auto';
-      ref.$refs.list.style.top = `${top + height}px`;
-      ref.$refs.list.style.left = `${left}px`;
+      if (ref.$refs.list.style.display !== 'none') {
+        ref.$refs.list.style.position = 'fixed';
+        ref.$refs.list.style.bottom = 'auto';
+        ref.$refs.list.style.top = `${top + height}px`;
+        ref.$refs.list.style.left = `${left}px`;
+      }
     },
   },
   mounted() {
     this.val = this.translateValueToOption(this.value);
     window.addEventListener('scroll', this.repositionDropDown);
+    if (document.getElementsByClassName('table-content')) {
+      document.getElementsByClassName('table-content')[0].addEventListener('scroll', this.repositionDropDown);
+    }
   },
   destroyed() {
     window.removeEventListener('scroll', this.repositionDropDown);
+    if (document.getElementsByClassName('table-content')) {
+      document.getElementsByClassName('table-content')[0].removeEventListener('scroll', this.repositionDropDown);
+    }
   },
 }
 </script>
