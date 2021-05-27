eff3c04c6e3b569dd51a60a1c2fbe8fa427fff6f
Felipinbombin <cephei.1313@gmail.com>
2021-05-27 17:27:59 -0400

    replace arrow image

--- src/components/StopTimesEditor.vue
+++ src/components/StopTimesEditor.vue
@@ -475,7 +475,7 @@ export default {
           'line-width': 3
         }
       });
-      let img = require('../assets/img/arrow-small.png')
+      let img = require('../assets/img/double-arrow.png')
       this.map.loadImage(img, (err, image) => {
         if (err) {
           console.log(err);
