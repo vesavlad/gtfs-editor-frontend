732f2be43180d8550718d37292de61cc628a0f7b
Felipinbombin <cephei.1313@gmail.com>
2021-06-11 23:26:00 -0400

    simpleinput does not use local value to remember v-model variable

--- src/components/vuetable/inputs/SimpleInput.vue
+++ src/components/vuetable/inputs/SimpleInput.vue
@@ -1,12 +1,12 @@
 <template>
-  <input ref="input" :type="field.type" v-model="val" :placeholder="`Enter ${field.title.toLowerCase()}`"
-         @input="$emit('input', $event.target.value)" @focus="localErrors=[]" :class="{error: hasErrors}"
+  <input :value="value" @input="$emit('input', $event.target.value)"
+         :type="field.type" :placeholder="`${field.title.toLowerCase()}`" @focus="localErrors=[]"
+         :class="{error: hasErrors}"
          v-tooltip="{ theme: 'error-tooltip', content: hasErrors?errors[0]:'', shown: hasErrors }"
          v-autowidth="{minWidth: 'calc(100% - 20px)'}"/>
 </template>
 
 <script>
-
 export default {
   name: 'SimpleInput',
   props: {
@@ -23,14 +23,10 @@ export default {
   },
   data() {
     return {
-      val: this.value,
       localErrors: []
     };
   },
   watch: {
-    value() {
-      this.val = this.value;
-    },
     errors() {
       this.localErrors = [...this.errors];
     }
